3d1242a26bf9d33f68d5d05fcdf4dae452b01a1f
Vicente Duch <vhduch@gmail.com>
2025-10-27 13:47:34 +0100

    [MTB-56] install basic dependencies and Tailwind CSS

--- client/meme-board/src/App.jsx
+++ client/meme-board/src/App.jsx
@@ -28,6 +28,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="bg-red-500 text-red p-4 m-4">
+  Si esto es rojo, Tailwind funciona
+</div>
     </>
   )
 }
---
+++ client/meme-board/tailwind.config.js
@@ -0,0 +1,8 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'], // archivos a escanear
+  darkMode: false, // 'media' o 'class' si quieres dark mode
+  theme: {
+    extend: {}, // aquí puedes extender colores, fuentes, etc.
+  },
+  plugins: [],
+};
