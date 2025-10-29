9cc412d924cda1a8feaf2b554c8c762570a6f350
Vicente Duch <vhduch@gmail.com>
2025-10-29 12:49:02 +0100

    Adjust text color at AboutPage and BoardPage to improve readability

--- client/meme-board/src/pages/AboutPage.jsx
+++ client/meme-board/src/pages/AboutPage.jsx
@@ -1,8 +1,8 @@
 export default function AboutPage() {
   return (
     <div className="text-center mt-8">
-      <h2 className="text-2xl font-semibold text-gray-700">Acerca de Meme Board 💡</h2>
-      <p className="mt-2 text-gray-500">Proyecto personal creado por Vicente Duch Moreno.</p>
+      <h2 className="text-2xl font-semibold text-white">Acerca de Meme Board 💡</h2>
+      <p className="mt-2 text-gray-400">Proyecto personal creado por Vicente Duch Moreno.</p>
     </div>
   );
 }
