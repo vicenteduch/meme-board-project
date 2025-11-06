a23e96b5300ba927c882c2aa6d7c483a2821fb36
Vicente Duch <vhduch@gmail.com>
2025-11-06 11:27:31 +0100

    [MTB-83] Added custom hooks useAuth() and useTasks() for cleaner context access

--- client/meme-board/src/hooks/useTasks.js
+++ client/meme-board/src/hooks/useTasks.js
@@ -1,4 +1,10 @@
 import { useContext } from 'react';
 import { TasksContext } from '../context/TasksContext.jsx';
 
-export const useTasks = () => useContext(TasksContext);
+export const useTasks = () => {
+  const context = useContext(TasksContext);
+  if (!context) {
+    throw new Error('useTasks must be used within a TasksProvider');
+  }
+  return context;
+};
